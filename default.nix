510c520fecb332ff4866305da127d0f70e9dc179
astk-hs <60435347+astk-hs@users.noreply.github.com>
2020-01-29 19:06:17 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> { config.allowUnfree = true; };
 
-haskellPackages.override {
-  overrides = self: super: {
+haskell.packages.ghc881.override {
+ overrides = self: super: {
     rotate = self.callCabal2nix "rotate" ./rotate {};
     rotateBackpack = self.callCabal2nix "rotateBackpack" ./rotateBackpack {};
     rotateUse= self.callCabal2nix "rotateUse" ./rotateUse{};
