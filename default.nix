5a2701f8a0c6c47c4971925a2f9c02eeb965c178
astk-hs <60435347+astk-hs@users.noreply.github.com>
2020-01-29 17:15:25 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-with import <nixpkgs> {};
+with import <nixpkgs> { allowUnfree = true; };
 
 haskellPackages.override {
   overrides = self: super: {
@@ -7,3 +7,9 @@ haskellPackages.override {
     rotateUse= self.callCabal2nix "rotateUse" ./rotateUse{};
   };
 }
+
+
+{-
+hask@LAPTOP-PQ3KPQAO:~/rotateDistributed/rotateDistributed$ nix-build default.nix
+error: anonymous function at /nix/store/kqsdinhsbvgv1x99hyn2w3z3pphi6077-nixpkgs-20.03pre210728.42a195919a1/nixpkgs/pkgs/top-level/default.nix:20:1 called with unexpected argument 'allowUnfree', at /nix/store/kqsdinhsbvgv1x99hyn2w3z3pphi6077-nixpkgs-20.03pre210728.42a195919a1/nixpkgs/pkgs/top-level/impure.nix:84:1
+-}
