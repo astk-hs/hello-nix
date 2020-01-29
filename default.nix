1887accc3968e38faddd27774fadb62e299b6459
astk-hs <60435347+astk-hs@users.noreply.github.com>
2020-01-29 19:15:32 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@ with import <nixpkgs> { config.allowUnfree = true; };
 
 haskell.packages.ghc881.override {
  overrides = self: super: {
-    rotate = haskell.lib.doJailbreak(self.callCabal2nix "rotate" ./rotate {};)
-    rotateBackpack = haskell.lib.doJailbreak(self.callCabal2nix "rotateBackpack" ./rotateBackpack {};)
-    rotateUse= haskell.lib.doJailbreak(self.callCabal2nix "rotateUse" ./rotateUse{};)
+    rotate = haskell.lib.doJailbreak(self.callCabal2nix "rotate" ./rotate {});
+    rotateBackpack = haskell.lib.doJailbreak(self.callCabal2nix "rotateBackpack" ./rotateBackpack {});
+    rotateUse= haskell.lib.doJailbreak(self.callCabal2nix "rotateUse" ./rotateUse{});
   };
 }
